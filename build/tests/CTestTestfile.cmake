ec2fe57d3a3894ffc6bc4bbaaeb5c8b6aecf1157
OM MILIND BADKAS <85434017+ombadkas@users.noreply.github.com>
2026-05-15 05:00:07 +0100

    Add files via upload

---
+++ build/tests/CTestTestfile.cmake
@@ -0,0 +1,36 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ombad/options-engine/tests
+# Build directory: C:/Users/ombad/options-engine/build/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test([=[PricerTests]=] "C:/Users/ombad/options-engine/build/tests/Debug/test_pricer.exe")
+  set_tests_properties([=[PricerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;7;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test([=[PricerTests]=] "C:/Users/ombad/options-engine/build/tests/Release/test_pricer.exe")
+  set_tests_properties([=[PricerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;7;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test([=[PricerTests]=] "C:/Users/ombad/options-engine/build/tests/MinSizeRel/test_pricer.exe")
+  set_tests_properties([=[PricerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;7;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test([=[PricerTests]=] "C:/Users/ombad/options-engine/build/tests/RelWithDebInfo/test_pricer.exe")
+  set_tests_properties([=[PricerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;7;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+else()
+  add_test([=[PricerTests]=] NOT_AVAILABLE)
+endif()
+if(CTEST_CONFIGURATION_TYPE MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+  add_test([=[GreeksTests]=] "C:/Users/ombad/options-engine/build/tests/Debug/test_greeks.exe")
+  set_tests_properties([=[GreeksTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;8;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+  add_test([=[GreeksTests]=] "C:/Users/ombad/options-engine/build/tests/Release/test_greeks.exe")
+  set_tests_properties([=[GreeksTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;8;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+  add_test([=[GreeksTests]=] "C:/Users/ombad/options-engine/build/tests/MinSizeRel/test_greeks.exe")
+  set_tests_properties([=[GreeksTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;8;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+elseif(CTEST_CONFIGURATION_TYPE MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+  add_test([=[GreeksTests]=] "C:/Users/ombad/options-engine/build/tests/RelWithDebInfo/test_greeks.exe")
+  set_tests_properties([=[GreeksTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;8;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+else()
+  add_test([=[GreeksTests]=] NOT_AVAILABLE)
+endif()
