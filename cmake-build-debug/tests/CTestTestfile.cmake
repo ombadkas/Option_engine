69593c44f2d4d912496f46a830a574379ed6cbd6
OM MILIND BADKAS <85434017+ombadkas@users.noreply.github.com>
2026-05-15 04:56:54 +0100

    Add files via upload

---
+++ cmake-build-debug/tests/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/ombad/options-engine/tests
+# Build directory: C:/Users/ombad/options-engine/cmake-build-debug/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[PricerTests]=] "C:/Users/ombad/options-engine/cmake-build-debug/tests/test_pricer.exe")
+set_tests_properties([=[PricerTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;7;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
+add_test([=[GreeksTests]=] "C:/Users/ombad/options-engine/cmake-build-debug/tests/test_greeks.exe")
+set_tests_properties([=[GreeksTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/ombad/options-engine/tests/CMakeLists.txt;8;add_test;C:/Users/ombad/options-engine/tests/CMakeLists.txt;0;")
